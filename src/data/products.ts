3285719777918685dbb0551c29d6c8bbdaec12c8
Minjun Park <minjunpark@snu.ac.kr>
2025-12-15 01:44:58 +0900

    change category thumbnail & apply break-keep

--- src/data/products.ts
+++ src/data/products.ts
@@ -7,15 +7,15 @@ export interface Categories {
 }
 export const categories: Categories = {
     "소방 제품": {
-        image: "https://www.masteco.co.kr/assets/images/main/section_2_img_3.jpg",
+        image: "/images/products/firefighting-thumbnail.png",
         description: "Commercial pumps for office buildings."
     },
     "분진폭발 방지 시스템": {
-        image: "https://www.masteco.co.kr/assets/images/main/section_2_img_3.jpg",
+        image: "/images/products/iep-thumbnail.png",
         description: "Commercial pumps for office buildings."
     },
     "플랜트 제어 시스템": {
-        image: "https://www.masteco.co.kr/assets/images/main/section_2_img_3.jpg",
+        image: "/images/products/yokogawa-thumbnail.png",
         description: "Commercial pumps for office buildings."
     },
 };
